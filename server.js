d560ea12f3b5c9b386fa3022789507ff2cc79fc6
Marko Koskinen <markokoskinen2037@gmail.com>
2020-01-24 15:55:55 +0200

    very nice api gateway

--- server.js
+++ server.js
@@ -1,20 +1,60 @@
 const express = require('express')
 const httpProxy = require('express-http-proxy')
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+
 const app = express()
 
 const userServiceProxy = httpProxy('https://sap-user-ms.herokuapp.com')
 const todoServiceProxy = httpProxy("https://sap-todo-ms.herokuapp.com")
 
+
+
+const validateToken = (req, res, next) => {
+    const authorizationHeaader = req.headers.authorization;
+    let result;
+    if (authorizationHeaader) {
+        const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
+        const options = {
+            expiresIn: '2d',
+            issuer: 'sap-todo'
+        };
+        try {
+            // verify makes sure that the token hasn't expired and has been issued by us
+            result = jwt.verify(token, process.env.JWT_SECRET, options);
+
+            // Let's pass back the decoded token to the request object
+            req.decoded = result;
+            // We call next to pass execution to the subsequent middleware
+            next();
+        } catch (err) {
+            // Throw an error just in case anything goes wrong with verification
+            console.log(err);
+
+            throw new Error(err);
+        }
+    } else {
+        result = {
+            error: `Authentication error. Token required.`,
+            status: 401
+        };
+        res.status(401).send(result);
+    }
+}
+
+
+
+
+
+
+
 // Authentication
 app.use((req, res, next) => {
-
-    let token
-    try {
-        token = req.headers.authorization.split(" ")[1].trim()
-        next()
-    } catch (e) {
-        res.send("Token missing / wrong format / expired / error / smth")
+    if (req.url === "/todos") {
+        validateToken(req, res, next)
     }
+    next()
 })
 
 // PROXY REQUESTS ONCE THE TOKEN REQUEST TOKEN HAS BEEN AUTHORIZED:
